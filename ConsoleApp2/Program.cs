bfdb69da0679134b244d72cd22a50aad270f5bd8
student <student@101-09>
2018-08-25 14:48:41 +0300

    +console

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,6 +11,9 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("klsdfj");
+            Console.WriteLine("lsdfjlaskdjfalskdfj\n\n\n\nlsdjflsdjflksjf");
+            Console.WriteLine("lsdfjlaskdjfalskdfj\n\n\n\nlsdjflsdjflksjf");
+            Console.Read();
         }
     }
 }
